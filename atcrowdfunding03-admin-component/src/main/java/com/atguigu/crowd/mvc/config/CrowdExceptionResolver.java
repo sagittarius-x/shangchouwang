0bbcf51db8ef68c5225d76209a2b7475924655ff
Frank Xie <frank.xie@sap.com>
2021-02-20 10:57:29 +0800

    Add front-end resource

--- atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/mvc/config/CrowdExceptionResolver.java
+++ atcrowdfunding03-admin-component/src/main/java/com/atguigu/crowd/mvc/config/CrowdExceptionResolver.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.atguigu.crowd.constant.CrowdConstant;
 import com.atguigu.crowd.util.CrowdUtil;
 import com.atguigu.crowd.util.ResultEntity;
 import com.google.gson.Gson;
@@ -16,15 +17,20 @@ import com.google.gson.Gson;
 //@ControllerAdvice 表示该类是一个基于注解的异常处理器类
 @ControllerAdvice
 public class CrowdExceptionResolver {
-    // @ExceptionHandler将一个具体的异常类型和一个方法关联起来
+	// @ExceptionHandler将一个具体的异常类型和一个方法关联起来
+
 	@ExceptionHandler(value = NullPointerException.class)
-	public ModelAndView resolveNullPointerException(NullPointerException exception,
-			HttpServletRequest request,
-			HttpServletResponse response
-			) {
-				
+	public ModelAndView resolveNullPointerException(NullPointerException exception, HttpServletRequest request,
+			HttpServletResponse response) {
+
+		String viewName = "system-error";
+		return commonResolve(viewName, exception, request, response);
+	}
+
+	private ModelAndView commonResolve(String viewName, Exception exception, HttpServletRequest request,
+			HttpServletResponse response) {
 		boolean requestType = CrowdUtil.judgeRequestType(request);
-		
+
 		if (requestType) {
 			ResultEntity<Object> resultEntity = ResultEntity.failed(exception.getMessage());
 			Gson gson = new Gson();
@@ -38,9 +44,9 @@ public class CrowdExceptionResolver {
 			return null;
 		}
 		ModelAndView modelAndView = new ModelAndView();
-		
-		modelAndView.addObject("exception", exception);
-		modelAndView.setViewName("system-error");
+
+		modelAndView.addObject(CrowdConstant.ATTR_NAME_EXCEPTION, exception);
+		modelAndView.setViewName(viewName);
 		return modelAndView;
 	}
 }
---
+++ utilproject/src/main/java/com/atguigu/crowd/constant/CrowdConstant.java
@@ -0,0 +1,9 @@
+package com.atguigu.crowd.constant;
+
+public class CrowdConstant {
+	public static final String MESSAGE_LOGIN_FAILED = "抱歉，账号密码错误，请重新输入";
+	public static final String MESSAGE_LOGIN_ACCT_ALREADY_INUSE = "The account has been in used";
+	public static final String MESSAGE_ACCESS_FORBIDEN = "Please login before you visit";
+	
+	public static final String ATTR_NAME_EXCEPTION = "exception";
+}
